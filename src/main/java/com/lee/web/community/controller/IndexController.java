bf223971e4ca176fc97ca9aabe7b5ed30d8333e3
ligaoyang <48485335+ligaoyang123@users.noreply.github.com>
2020-03-20 10:16:41 +0800

    community

---
+++ src/main/java/com/lee/web/community/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.lee.web.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Company MySteelSoft
+ * @Author GaoYang Lee
+ * @Date Create in 2020/3/18 15:19
+ * @Description This is a good day
+ **/
+@Controller("/user")
+public class IndexController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "";
+    }
+
+}
